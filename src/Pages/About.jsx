a7a7da4ffe4cb7483b3957b00bb4ddd3366ad670
Jay Doctor <jaydoctr104@gmail.com>
2025-04-22 12:09:22 +0200

    Changed CV Url

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -247,7 +247,7 @@ const AboutPage = () => {
             </p>
 
             <div className="flex flex-col lg:flex-row items-center lg:items-start gap-4 lg:gap-4 lg:px-0 w-full">
-              <a href="https://drive.google.com/file/d/1Ooh2krgCEppsKnJup5EhUinX4gZPItPI/view?usp=sharing" className="w-full lg:w-auto">
+              <a href="https://drive.google.com/file/d/1i04L37t_YvywDuhusvauTfLCyeN1yA8X/view?usp=sharing" className="w-full lg:w-auto">
                 <button
                   data-aos="fade-up"
                   data-aos-duration="800"
